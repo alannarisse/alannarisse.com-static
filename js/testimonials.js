65783b7c9bec771d675aef1bbe86bdf643c8319b
alannarisse <alannarisse@users.noreply.github.com>
2024-09-09 17:32:15 -0700

    adding more things back

---
+++ js/testimonials.js
@@ -0,0 +1,96 @@
+DATA = [
+  {
+    testimonial: `I hope, someday, to be as interesting as Alanna Risse.`,
+    by: 'The Most Interesting Man in the World'
+  },
+  {
+    testimonial: `Alanna is a fabricator of absurdity. She pushes life into the delicate balance between truth and humor often resulting in beautiful disharmony. Alanna is unusually driven and wonderfully unusual in that she can accomplish and learn anything that is put in front of her.`,
+    by: 'Liz Randall'
+  },
+  { testimonial: 'Alanna is a rare mix of skill, determination, and goof. Her technical chops are top-notch, and she is able to learn new skills with speed and grace. She puts her heart into her work.', by: 'Lucas Haley' },
+  {
+    testimonial: 'Alanna knows front-end/templates better than anybody. And she gets the job done well and gets it done fast.',
+    by: 'Alan Peters'
+  },
+  {
+    testimonial: `Alanna is a tremendous asset to any company she works for. Not only is she incredibly technically savvy, she's extremely creative and can come up with new, better solutions to existing problems. I loved working with her and someday hope I get the chance to work with Alanna again.`,
+    by: 'Lynn Beighley'
+  },
+  { testimonial: `Alanna was the life and soul of the party at Apple in Cork when she was seconded from "home base" Cupertino for a period of time. It was great to have such positive experience, especially in the Multimedia Application Space, in the group and it is no surprise that she has gone on to bigger and better things in the last 10 years.`, by: 'Gavin Costello' },
+  { testimonial: `Hey you! Wanted to let you know that over the years this has always been true: Whenever I see your name pop up, I smile. You're one of the warmest, funniest people I've ever come across. Thanks for reminding me you're still around! Made my day a little brighter.`, by: 'Michael Hujsak, collegue at Apple 1995-97' },
+  { testimonial: `Good, timely work; cheerful in the face of adversity, a pleasure to work with and be around. All-around skilled as well as talented.`, by: 'Kaolin Fire' },
+  {
+    testimonial: `Alanna = 1, React Forms = 0`,
+    by: 'Alanna Risse'
+  },
+  { testimonial: `I’ve known Alanna for nearly a decade now and, honestly, my life just keeps getting better. I mean, I don’t actually KNOW know her (like in the biblical sense) but she breathed on me once back in art school and I was totally into it. Pretty sure she was too. What I mean is she changed my life. Made me sparkly and intoxicating. Everywhere I go, people adore me... but Alanna loved me first. I’m forever changed and forever grateful.`, by: 'Angela Simione' },
+  {
+    testimonial: `I can't explain enough how impactful you have been to me as a colleague and guide for our students!!
+	`,
+    by: 'Emma Holland'
+  },
+  {
+    testimonial: `Thank you for all of your help during the term and introducing us into the bumpy, breezy bike ride of coding, stay awesome!`,
+    by: 'Brandon Pahnish, Student, PSU'
+  },
+  {
+    testimonial: `Alanna so sad to hear this!! Absolute best of luck in everything you do, loved when we had the opportunity to chat over zoom recently about your involvement with the programs :orange_heart: thank you for everything you've done for the programs!! -Linda, Thinkful Educator Recruiter`,
+    by: 'Linda Huard  1/27/21 Thinkful Educator Recruiter'
+  },
+  {
+    testimonial: `I/others on my team are heartbroken by your announcement. I hope, hope, hope that your next adventure is off to a great start!
+	`,
+    by: 'Nicholas Martin  12:21 PM Academic Success Manager'
+  },
+  {
+    testimonial: `I will absolutely miss you - your feedback and support have really helped me grow as a designer. I’ll send you a celebration message when I graduate :party-blob: Best of luck with starting your new endeavors!`,
+    by: 'Jennifer Lundeen  12:24 PM Mentee Bloc UX/UI program'
+  },
+  {
+    testimonial: `It was a wild year, but i'm doing well, and a large part of that is because of the job I was able to land. So I truly appreciate everything you taught me, and making that opportunity possible. -Alex N Walters, Mentee, Thinkful
+	`,
+    by: 'alex walters 1/28/21'
+  },
+  {
+    testimonial: `Thank you for all that you've done for Thinkful @Alanna Risse :orange_heart: all the best to you in your ventures beyond here!! 
+	`,
+    by: '--Kara Gillette Manager of Outcomes Specialists'
+  },
+  {
+    testimonial: `Alanna so sad to hear you are leaving!! Absolute best of luck in everything you do, loved when we had the opportunity to chat over zoom recently about your involvement with the programs :orange_heart: thank you for everything you've done for the programs!! `,
+    by: '--Linda Huard Educator Recruiter'
+  },
+  {
+    testimonial: `Alanna is a fantastic teacher and mentor. Having a tremendous amount of experience in web development and art she can provide an answer and a resource for every question you might have. I've received a lot of knowledge about the use of typefaces, colors, design tools, as well as working with GitHub, programming in HTML, CSS, JS etc. from our meetings. I highly recommend Alanna as a mentor and a teacher! `,
+    by: '--Inna Beltcova, Associate UX Designer at AllScripts March 2019'
+  },
+  {
+    testimonial: `Thank you so much! I appreciate all that you have done!`,
+    by: 'Jenny Wong feb 22, 2021'
+  },
+  {
+    testimonial: `Thanks for the awesome two classes Alanna! As frustrating has coding has been, it's been a good experience to have with you as my teacher. I hope you have a great spring break!`,
+    by: 'Anitra Meyer 3/16/21'
+  },
+  {
+    testimonial: `I wanted to connect with you and say thanks again for mentoring me through the Thinkful pre-work. I recently graduated from General Assembly's software engineering program and thought about how helpful you were at the beginning of my journey and just wanted to say thanks!`,
+    by: 'Sarah Moe 11/30/21'
+  },
+  {
+    testimonial: `I am learning a lot and your videos are super super helpful, I can't thank you enough for recording the classes and creating more videos to explain harder concepts. 
+	`,
+    by: 'Haley Watts 2/18/22'
+  },
+  {
+    testimonial: `This class has been super rewarding for me! I had a lot of fun and learned a lot 🥳🥳 Thank you Alanna!`,
+    by: 'Haley Watts 3/7/22'
+  },
+  {
+    testimonial: `Alanna, You're amazing!!! What an awesome and hilarious and touching and generous and thoughtful gift you dropped off for me today. When I walked into the office, all my coworkers were dancing and singing and clapping and giving me a standing ovation as they pointed to my desk and showed me what you brought. It was both crazy embarrassing and amazingly awesome at the same time. Thank you for thinking of me!`,
+    by: 'Dennis Howe PT 4/26/2022'
+  },
+  {
+    testimonial: `I have really been enjoying your class and you’ve quickly become one of my favorite professors!`,
+    by: 'Sam Baltierra 10/30/22'
+  }
+]
